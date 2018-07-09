5ac125252d02da9d1efd6ff5b7358e115e47bfcc
Amine Badraoui <aminebadraoui.contact@gmail.com>
2018-07-08 23:11:20 -0400

    Updated the model for mapping

--- ShopifyKit/ShopifyKit/Models/CollectionModel.swift
+++ ShopifyKit/ShopifyKit/Models/CollectionModel.swift
@@ -1,27 +1,31 @@
 //
-//  ProductModel.swift
+//  CollectionModel.swift
 //  ShopifyKit
 //
-//  Created by Amine on 2018-06-25.
+//  Created by Amine on 2018-07-02.
 //  Copyright © 2018 aminebadraoui. All rights reserved.
 //
 
 import Foundation
 import MobileBuySDK
 
-
-public struct Product  {
-    public var model: Storefront.Product
+public class Collection {
+    
+    public var model: Storefront.Collection
     public var title: String
     public var handle: String
     public var description: String
+    public var imageUrl: URL?
     public var id: String
-    
-    public init(from model: Storefront.Product) {
+   
+    public init(from model: Storefront.Collection) {
         self.model = model
         self.title = model.title
         self.handle = model.handle
         self.description = model.description
+        self.imageUrl = model.image?.originalSrc
         self.id = model.id.rawValue
     }
+    
+    
 }
